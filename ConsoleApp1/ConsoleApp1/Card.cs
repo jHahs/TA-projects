112ff00756a734dc4cb64c9c08ec2ca4d20b6a31
Josh Hahs <joshua.hahs28@gmail.com>
2018-06-04 22:41:04 -0700

    submit Console App for Method
    submit Console App for Method

---
+++ ConsoleApp1/ConsoleApp1/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+	public class Card
+	{
+		public Card()
+		{
+			 
+		}
+		public string Suit { get; set; }
+		public string Face { get; set; }
+
+	}
+}
