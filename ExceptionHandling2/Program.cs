9b74f39b1526813ed6a14c59b434aa811f8e834f
Josh Hahs <joshua.hahs28@gmail.com>
2018-06-18 18:29:34 -0700

    Exception Handling excercise

---
+++ ExceptionHandling2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ExceptionHandling2
+{
+	class Program
+	{
+		static void Main()
+		{
+			Program:
+			try
+			{
+				bool validAge = false;
+				int age = 0;
+				while (!validAge)
+				{
+					Console.WriteLine("Please enter your age.");
+					validAge = int.TryParse(Console.ReadLine(), out age);
+					if (age == 0)
+					{
+						throw new ZeroException();
+					}
+					else if (age < 0)
+					{
+						throw new NegativeException();
+					}
+
+				}
+				var currentyear = DateTime.Now.Year.ToString();
+				int birth = Int32.Parse(currentyear) - age;
+				Console.WriteLine("You were born in the year {0}", birth);
+				Console.ReadLine();
+			}
+			catch(ZeroException)
+			{
+				Console.WriteLine("Please enter a number higher than zero");
+				goto Program;
+			}
+			catch(NegativeException)
+			{
+				Console.WriteLine("Please enter a number that is not a negative");
+				goto Program;
+			}
+			catch(Exception)
+			{
+				Console.WriteLine("Please enter a Whole number");
+				goto Program;
+			}
+			Console.ReadLine();
+
+			//Console.WriteLine("Please enter your age.");
+			//int age = Convert.ToInt32(Console.ReadLine());
+			//var currentyear = DateTime.Now.Year.ToString();
+			//int birth = Int32.Parse(currentyear) - age;
+			//Console.WriteLine("You were born in the year {0}", birth);
+			//Console.ReadLine();
+		}
+	}	
+}
